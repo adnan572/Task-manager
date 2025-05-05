a594a41f9cbc9dd4ebd1f2295a09fd7e70251a69
adnan <kamaladnan@gmail.com>
2025-05-05 20:51:40 +0100

    added some changes

--- models/task.py
+++ models/task.py
@@ -1,1 +1,19 @@
-#This file contains the task class
+#Add a task 
+# Remove a task
+# Edit a task
+# Add a description
+# Edit a description
+
+#intial tasks list
+tasks = []
+
+def addTask(all_tasks):
+	new_task = input("Add a task: ")
+	all_tasks.append(new_task)
+
+	for task in all_tasks:
+		print(task)
+
+
+ #start application
+addTask(tasks)
