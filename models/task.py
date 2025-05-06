c0365f38afdf2532106f0d0e0654cdbde74bd8f6
adnan <kamaladnan@gmail.com>
2025-05-06 15:44:42 +0100

    added 3 functions: add a task, new operation and display all the tasks

--- models/task.py
+++ models/task.py
@@ -7,12 +7,30 @@
 #intial tasks list
 tasks = []
 
+def displayTasks(yourTasks):
+	print('\nYour tasks: ')
+	for index, task in enumerate(yourTasks):
+		print(f'{index + 1} : {task}')
+
+def newOperation(new):
+	operation = input("press 'A' to add a new task, 'E' to edit a task, 'R' to remove a task or 'F' to quit the application: ")
+	if operation == 'A':
+		addTask(new)
+	elif operation == 'E':
+		pass
+	elif operation == 'R':
+		pass
+	elif operation == 'F':
+		return
+	else:
+		newOperation(new)
 def addTask(all_tasks):
 	new_task = input("Add a task: ")
 	all_tasks.append(new_task)
 
-	for task in all_tasks:
-		print(task)
+	displayTasks(all_tasks)
+	newOperation(all_tasks)
+
 
 
  #start application
